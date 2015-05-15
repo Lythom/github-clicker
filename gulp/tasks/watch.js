28e6d137870f91658f7474b81217529f54b8e3c6
Samuel BOUCHET <samuel.bouchet@gmail.com>
2015-05-15 16:35:46 +0200

    update watch to build every lunch.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
 
-gulp.task('watch', function() {
+gulp.task('watch', ['build'], function() {
 
   // Watch for changes in frontend js and run the 'javascript' task
   gulp.watch([
